f1290335a0659dd6a9d9eb462f9510290fcc9a3f
samoila dan robert <danrobertsamoila@gmail.com>
2021-03-14 16:16:58 +0200

    Removed diagonal movement
    Changed logic to go on Y axis first then x in order to make the character not move diagonally

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Vector2 targetLocation;
+    public Vector2 yAxisTargetLocation;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +15,17 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.position = Vector2.MoveTowards(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y),
-               targetLocation, 3 * Time.deltaTime);
+        //xtarget x=0 y=1
+        if(targetLocation.y!=gameObject.transform.position.y)
+        {
+            gameObject.transform.position = Vector2.MoveTowards(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y),
+                   new Vector2(gameObject.transform.position.x, targetLocation.y), 3 * Time.deltaTime);
+        }
+        else
+        {
+            gameObject.transform.position = Vector2.MoveTowards(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y),
+                   targetLocation, 3 * Time.deltaTime);
+        }
     }
 
     public void Killed()
@@ -28,7 +38,7 @@ public class PlayerMovement : MonoBehaviour
         {
 
             //gameObject.transform.position = new Vector3(position.x, position.y, -1);
-         
+
             targetLocation = new Vector2(position.x,position.y);
             CombatProcessor.Instance.TurnCombat(this.gameObject);
 
